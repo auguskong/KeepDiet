28de9d1f3def14d487be5fcf37ca3fb548b3bf26
Auguskong <xiangyukongdaily@gmail.com>
2018-03-24 16:13:58 +0000

    finish the DB setting, show the groups from the DB

--- Main/app.js
+++ Main/app.js
@@ -1,20 +1,31 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var moment = require("moment");
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    //User            = require("./models/user"),
+    moment          = require("moment");
 
+mongoose.connect("mongodb://localhost/keep_diet");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var groups = [
-        {name: "Running Groups", image: "http://watchfit.com/wp-content/uploads/2015/10/24489910_l-1024x682.jpg"},
-        {name: "Camping Groups", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Hiking Groups", image: "https://static1.squarespace.com/static/53bed66ee4b0fc5deee88551/t/56b7c428555986e5db03075a/1454883907809/trail-hiking-mountain-group"},
-        {name: "Cycling Groups", image: "http://www.lsu.edu/highlights/2010/07/images/CyclingClub2.jpg"},
-        {name: "Swimming Groups", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8tTH5bJQYWSqgoClrXIH-8QRuBbswnOH4lycbLbMmaynoHZY3GA"},
-        {name: "Boxing Groups", image: "http://harlemboxing.nyc/wp-content/uploads/2014/07/Group_sm1.jpeg"},
-        {name: "Basketball Groups", image: "http://www.loganleisurecentres.com.au/__data/assets/image/0011/439967/Jazz-Team-Training-8-16-Cornubia-Park.jpg"}
-];
+//SCHEMA SETUP
+var groupSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Group = mongoose.model("Group", groupSchema);
+
+// var groups = [
+//         {name: "Hiking Groups", image: "https://static1.squarespace.com/static/53bed66ee4b0fc5deee88551/t/56b7c428555986e5db03075a/1454883907809/trail-hiking-mountain-group"},
+//         {name: "Cycling Groups", image: "http://www.lsu.edu/highlights/2010/07/images/CyclingClub2.jpg"},
+//         {name: "Swimming Groups", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8tTH5bJQYWSqgoClrXIH-8QRuBbswnOH4lycbLbMmaynoHZY3GA"},
+//         {name: "Boxing Groups", image: "http://harlemboxing.nyc/wp-content/uploads/2014/07/Group_sm1.jpeg"},
+//         {name: "Basketball Groups", image: "http://www.loganleisurecentres.com.au/__data/assets/image/0011/439967/Jazz-Team-Training-8-16-Cornubia-Park.jpg"}
+// ];
     
 app.get("/", function(req, res){
     res.render("Landing/landing");
@@ -29,17 +40,29 @@ exports.index= function(req, res) {
 }
 
 app.get("/groups", function(req, res){
-    res.render("Groups/groups",{groups:groups});
+    //GET all groups from DB
+    Group.find({}, function(err, groups){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("Groups/groups", {groups: groups})
+        }
+    })
 });
 
 app.post("/groups", function(req, res){
     // get data from form and add to groups array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    groups.push(newCampground);
-    //redirect back to groups page
-    res.redirect("/groups");
+    var newGroup = {name: name, image: image}
+    //Create a new group and save to DB
+    Group.create(newGroup, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/groups");
+        }
+    });
 });
 
 app.get("/groups/new", function(req, res){
@@ -55,5 +78,5 @@ app.get("/groups/desc", function(req, res){
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+   console.log("The Server Has Started!");
 });
